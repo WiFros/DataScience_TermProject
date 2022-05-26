2435ac59d4e5d0da669101d94c978ae93a997349
dong <idh1007@naver.com>
2022-05-26 22:54:16 +0900

    진행중

--- PJY/TermProject.py
+++ PJY/TermProject.py
@@ -4,26 +4,53 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 plt.style.use("ggplot");
-sns.set(rc = {'figure.figsize':(12,7)})
+sns.set(rc={'figure.figsize': (12, 7)})
 
-#Encoder
+# Encoder
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import OrdinalEncoder
 
-#Test_train split
+# Test_train split
 from sklearn.model_selection import train_test_split
 
-#Scaler
+# Scaler
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import RobustScaler
 
-#Algorithm
+# Algorithm
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 
-#Evaluation
-from sklearn
+# Evaluation
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_curve
+from sklearn.metrics import confusion_matrix
+
+from sklearn.model_selection import StratifiedKFold as skf
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
 
 
+
+# exploratory data analysis
+df = pd.read_csv("../star_classification.csv")
+print(df.head())
+print(df.tail())
+print(df.info())
+print(df.isnull().sum())
+print(df["class"].value_counts())
+print(df.describe())
+
+sns.countplot(df["class"], palette="Set3")
+plt.title("Class ", fontsize=10)
+plt.show()
+df["class"] = [0 if i == "GALAXY" else 1 if i == "STAR" else 2 for i in df["class"]]
+
+f, ax = plt.subplots(figsize=(12, 8))
+sns.heatmap(df.corr(), cmap="PuBu", annot=True, linewidths=0.5, fmt='.2f', ax=ax)
+plt.show()
+
+corr = df.corr()
+corr["class"].sort_values()
